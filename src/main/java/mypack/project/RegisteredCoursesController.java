5443697da72feb93057965d9dee6c868cee1771e
cabbage <shakun650@gmail.com>
2023-03-05 20:43:30 +0600

    Fixed Buttons of registeredCoursePage page

--- src/main/java/mypack/project/RegisteredCoursesController.java
+++ src/main/java/mypack/project/RegisteredCoursesController.java
@@ -1,5 +1,6 @@
 package mypack.project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
@@ -8,12 +9,15 @@ import userPack.Courses;
 import userPack.Student;
 import userPack.User;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class RegisteredCoursesController {
     @FXML
     private VBox course_box;
+    @FXML
+    private Label full_name_label;
     private User currentUser;
 
     private Student currentStudent;
@@ -23,6 +27,7 @@ public class RegisteredCoursesController {
         currentStudent=student;
         registered_courses=courses;
         currentUser=user;
+        full_name_label.setText(currentStudent.getName());
 
         for(int i=0 ; i<registered_courses.size() ; i++){
             Pane pane =new Pane();
@@ -34,5 +39,22 @@ public class RegisteredCoursesController {
             pane.getChildren().add(vBox);
             course_box.getChildren().add(pane);
         }
+    }
+
+    public void progressBtnClicked (ActionEvent event) throws SQLException, IOException {
+        StudentDashBoardController studentDashBoardController=new StudentDashBoardController();
+        studentDashBoardController.assignDummyController(currentStudent, registered_courses, currentUser);
+        studentDashBoardController.progressBtnClicked(event);
+    }
+    public void courseBtnClicked (ActionEvent event) throws SQLException, IOException {
+        System.out.println("Currently in course registration page");
+    }
+    public void resultBtnClicked (ActionEvent event){
+        System.out.println("Result btn for progress not done");
+    }
+    public void logoutBtnClicked (ActionEvent event) throws SQLException, IOException {
+        StudentDashBoardController studentDashBoardController=new StudentDashBoardController();
+        studentDashBoardController.assignDummyController(currentStudent, registered_courses, currentUser);
+        studentDashBoardController.logoutBtnClicked(event);
     }
 }
