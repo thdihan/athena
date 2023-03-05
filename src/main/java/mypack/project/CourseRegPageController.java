555263dcf63e0daffff4c2c651653b23f1c3a486
Tanvir Hossain Dihan <thdihan@outlook.com>
2023-03-04 20:33:14 -0800

    One problem in Course Registration Button

--- src/main/java/mypack/project/CourseRegPageController.java
+++ src/main/java/mypack/project/CourseRegPageController.java
@@ -2,11 +2,17 @@ package mypack.project;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import userPack.Courses;
 import userPack.Student;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -35,9 +41,21 @@ public class CourseRegPageController {
 //
 //    }
 
-    public void registerCourseBtnClicked(ActionEvent event) throws SQLException {
+    public void registerCourseBtnClicked(ActionEvent event) throws SQLException, IOException {
         DbUtilities dbUtilities=new DbUtilities();
         dbUtilities.registerCourses(course_box, currentStudent, offered_courses);
         System.out.println("Registered");
+
+
+        // ERROR : Button clicked should go to the registeredCourse page and it goes but no data shown. Fix it.
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("registeredCourses.fxml"));
+        Parent root = loader.load();
+
+        Scene registeredCourseScene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(registeredCourseScene);
+        window.show();
     }
+
 }
